934c3f6372dee47037d605e49c69f04d3fb8df20
Arthur <cesar.velcas23@gmail.com>
2025-11-18 17:18:42 -0600

    Entrega corregida de la practica 4

--- Practica 4/Practica 4.c
+++ Practica 4/Practica 4.c
@@ -10,12 +10,8 @@ int main(void) {
     printf("##### Paso 1 #####\n");
     printf("Cuantos enteros deseas almacenar? ");
     scanf("%d", &n);
-
     arr = (int *)malloc(n * sizeof(int));
-    if (arr == NULL) {
-        printf("No se pudo reservar memoria con malloc\n");
-        return 1;
-    }
+    
 
     printf("Valores iniciales (basura):\n[");
     for (i = 0; i < n; i++) {
@@ -27,11 +23,7 @@ int main(void) {
     printf("##### Paso 2 #####\n");
     printf("Reservar memoria con calloc\n");
     arr2 = (int *)calloc(n, sizeof(int));
-    if (arr2 == NULL) {
-        printf("Error, no se puede reservar memoria con calloc\n");
-        free(arr);
-        return 1;
-    }
+    
 
     printf("Valores iniciales en arr2 (calloc, ceros):\n[");
     for (i = 0; i < n; i++) {
@@ -56,17 +48,12 @@ int main(void) {
     //Paso 4: Redimensionar con realloc
     printf("##### Paso 4 #####\n");
     printf("Redimensionar con realloc\n");
-    printf("Ingrese una nueva medida para el arreglo? "); //Queria usar la palabra "tamaño" pero me marcaba error
+    printf("Ingrese una nueva medida para el arreglo"); //Queria usar la palabra "tamaño" pero me marcaba error
     scanf("%d", &m);
 
     int *tmp = (int *)realloc(arr2, m * sizeof(int));
-    if (tmp == NULL) {
-        printf("Error: no se pudo redimensionar memoria.\n");
-        free(arr);
-        free(arr2);
-        return 1;
-    }
     arr2 = tmp;
+
 
     // Si el arreglo es más grande, pedir los valores que faltan
     if (m > n) {
@@ -91,3 +78,16 @@ int main(void) {
 
     return 0;
 }
+
+/* 
+1. ¿Qué diferencia observaron entre los valores iniciales con malloc y calloc?
+En malloc los valores iniciales son basura (valores aleatorios), mientras que en 
+calloc los valores iniciales son ceros.
+2. ¿Qué sucede si en realloc piden un tamaño más grande que el original? ¿y más
+pequeño?
+Si pide un tamaño más grande, el contenido original se mantiene 
+y el espacio adicional queda como basura. Si pide un tamaño más pequeño, el contenido se pierde
+cualquier dato que excede el limite.
+3. ¿Qué pasa si olvidan llamar a free?
+Provocamos una fuga de memoria, "memory leak", lo que la memoria empieza a consumir RAM innecesariamente
+*/
