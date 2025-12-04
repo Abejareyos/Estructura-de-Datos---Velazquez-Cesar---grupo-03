44c58e6b0d0272c231c48109667d7a91be33223f
Arthur <cesar.velcas23@gmail.com>
2025-12-03 21:54:09 -0600

    Introduccion a Python.py

--- Practica 9/Introduccion a Python.py
+++ Practica 9/Introduccion a Python.py
@@ -55,8 +55,7 @@ print((11*3)+2 == 36 -1) #Verdadero
 print("curso" != "CuRsO") #Verdadero
 
 #Declaracion de una lista simple
-lista_diasDelMes=[31,28,31,30,31,30,31,31,30,31,30.31]
-
+lista_diasDelMes=[31,28,31,30,31,30,31,31,30,31,30,31]
 print(lista_diasDelMes) #Imprime lista completa
 print(lista_diasDelMes[0]) #Imprime elemento 1
 print(lista_diasDelMes[6]) #Imprime elemento 7
@@ -76,8 +75,8 @@ print(lista_numeros[1][1])
 print(lista_numeros[1][2])
 
 #Se cambio el valor de uno de los elementos de la lista
-lista_numeros[5][0] = "Cinco"
-print(lista_numeros[5])
+#lista_numeros[5][0] = 'Cinco'
+#print(lista_numeros[5])
 
 #Declaracion de una tupla
 tupla_diasDelMes=(31,28,31,30,31,30,31,31,30,31,30,31)
@@ -100,12 +99,10 @@ print(tupla_numeros[1][1])
 print(tupla_numeros[1][2])
 
 #Probando la mutabilidad de las listas vs la no mutabilidad de las tuplas
-print("valor actual {}".format(lista_diasDelMes[0]))
+print("valor_actual {}".format(lista_diasDelMes[0]))
 lista_diasDelMes[0] = 50
-print("valor cambiado {}".format(lista_diasDelMes[0]))
+print("valor_cambiado {}".format(lista_diasDelMes[0]))
 # tupla_diasDelMes[0] = 50 Esta asignacion mandaria un error porque las tuplas no se pueden cambiar su valor
-
-
 #Se debe importat la libreria para hacer uso de namedtuple
 from collections import namedtuple
 #Se crea la tupla con nombre
@@ -121,6 +118,142 @@ planeta2 = planeta('Venus', 2)
 #Usando la referencia se llama a cada uno de sus campos
 print(planetal.nombre, planetal.numero)
 print(planeta2[0],planeta2[1])
+print('Campos de la tupla: {}'.format(planeta._fields))
+
+#Creando un diccionario
+elementos = { 'hidrogeno': 1, 'helio': 2, 'carbon': 6 }
+
+#El momento de la impresión, pueden aparecer en diferente orden del introducido
+print(elementos)
+print(elementos['hidrogeno'])
+
+#Se puede agregar elementos al diccionario
+elementos['litio'] = 3
+print(elementos) #Imprime todos los elementos, ahora que los elementos no están ordenados
+
+#Creando un diccionario con valores
+peso_elementos = { 'hidrogeno': {'numero': 1, 'peso': 1.00794},
+                   'helio': {'numero': 2, 'peso': 4.002602},
+                   'carbon': {'numero': 6, 'peso': 12.0107} }
+print(peso_elementos)
+print(peso_elementos['hidrogeno'])
+print(peso_elementos['hidrogeno']['peso'])
+
+#Añadiendo el valor de un elemento
+peso_elementos['hidrogeno']['peso'] = 1.00786
+print(peso_elementos['hidrogeno'])
+
+#Creando una llave
+peso_elementos['hidrogeno']['gas noble'] = True
+print(peso_elementos['hidrogeno'])
+
+#Recorrer todos los elementos del diccionario
+for llave in peso_elementos:
+    print(llave, peso_elementos[llave]['numero'], peso_elementos[llave]['peso'])
+
+#Las funciones pueden recibir n número de parámetros, no se necesita indicar el tipo
+def imprime_nombre(nombre):
+    print('hola ' + nombre)
+
+#Llamada a la función
+imprime_nombre('JJ')
+
+#Definiendo una función que regrese el cuadrado de un número
+def cuadrado(x):
+    return x * x
+
+#La función format() sirve para convertir los parámetros que recibe, en cadenas; decora valores
+def imprime_valores(val1, val2, val3):
+    print('1: {} 2: {} 3: {}'.format(val1, val2, val3))
+
+#Función con un valor por defecto
+def cuadrado_def(x=2):
+    return x * x
+
+print(cuadrado_def())
+
+#Variables globales
+global_var = 'Global'
+print(global_var)
+
+def imprime_global():
+    print(global_var)
+
+imprime_global()
+
+#Se crea una variable local que tiene el mismo nombre que la variable global
+def funcion_vg1():
+    vg = 'local'
+    print(vg)
+
+
+#Para resolver el problema anterior y especificar que no quiero hacer uso de la
+#palabra reservada global
+def funcion_vg():
+    global vg
+    vg = 'Local'
+    print(vg)
+
+funcion_vg()
+print(vg)
+
+#Declaraciones if-else
+def elementoMayor(param1, param2):
+    if param1 > param2:
+        print(param1)
+    else:
+        print(param2)
+
+#Declaraciones if-elif-else
+def numero(n):
+    if n == 1:
+        print('n es 1')
+    elif n == 2:
+        print('n es 2')
+    else:
+        print('No hay opción')
+
+#Ciclo while
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+#Iteración en listas
+for n in [1,2,3,4,5]:
+    print(n)
+
+for n in range(5):
+    print(n)
+
+for n in range(-3,2):
+    print(n)
+
+for n in ["uno", "dos", "tres", "cuatro"]:
+    print(n)
+
+#Iteración en diccionarios
+elementos = { 'hidrogeno': 1, 'helio': 2, 'carbon': 6 }
+for llave, valor in elementos.items():
+    print(llave, valor)
+
+#Uso de librerías
+import math
+a = math.cos(math.pi)
+print(a)
+
+from math import pi
+b = math.cos(pi)
+print(b)
+
+#Gráfica con matplotlib
+import matplotlib.pyplot as plt  
+import numpy as np
+x = np.arange(-10, 10, 0.5)
+y = x**2
+plt.plot(x, y, 'ro')
+plt.title('Puntos')
+plt.show()
 
 
 
